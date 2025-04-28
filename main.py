d78b0bcb0e4c5612fc31cc756ff994b7f6a99029
Alex-C-Mwangi <149400745+Alex-C-Mwangi@users.noreply.github.com>
2025-04-28 07:26:37 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from pet import Pet
 def main():
     if __name__ == "__main__":
-        my_pet = Pet("Phany")
+        my_pet = Pet("Phany") #Giving pet name.
+        my_pet.get_status() #Checks the hunger, energy and happiness of pet
+        my_pet.eat() #Testing eat method
         my_pet.get_status()
-        my_pet.eat()
-        my_pet.get_status()
-        my_pet.train("Jump")
-        my_pet.show_tricks()
+        my_pet.train("Jump") #Training new trick
+        my_pet.show_tricks() #Shows ist of tricks learnt
 main()
